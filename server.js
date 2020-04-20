6fc95130e2a5eb025cd0db0450d46fd8d7ea60f1
Emmett Brady <emmett.brady@gmail.com>
2020-04-20 09:13:44 -0700

    wrote app.post in server, added uuid npm for random id

--- server.js
+++ server.js
@@ -2,6 +2,9 @@
 const express = require('express');
 const fs = require('fs');
 const path = require('path');
+const databasePath = path.join(__dirname, "/db/db.json");
+// uuid generates random ids
+const uuid = require('uuid/v4');
 const database = require("./db/db.json");
 
 // set up express app
@@ -24,13 +27,25 @@ app.get("/notes", function(req, res) {
     res.sendFile(path.join(__dirname, "/public/notes.html"));
   });
 
-// api/notes
+// return all saved notes in database as JSON
 app.get("/api/notes", function(req, res) {
     res.json(database);
   });
 
 //app.post
+app.post("/api/notes", function (req, res) {
   // add a random id to each note and save it to database
+  const randomID = uuid();
+  const newNote = req.body;
+  newNote.id = randomID;
+
+  // push new task with random id to database
+  database.push(newNote);
+  fs.writeFileSync(databasePath, JSON.stringify(database), function(err, data) {
+      if (err) throw err;
+  })
+  res.json(newNote);
+});
 
 //app.delete
   // loop through database to find id so note can be deleted
